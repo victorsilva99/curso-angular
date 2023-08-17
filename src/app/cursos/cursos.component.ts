1729ad32ed41cb40efd437d4f5b7c597f447a59b
Victor Silva <fe605@t-systems.com.br>
2023-08-17 13:42:07 -0300

    feat: create templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+
+  nomeSite: string;
+
+  constructor() {
+    this.nomeSite = 'http://www.google.com';
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
