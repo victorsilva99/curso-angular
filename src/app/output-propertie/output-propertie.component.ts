23b55cc884fdd94c881cdfdbfa33a1a2eef919a3
Victor Silva <fe605@t-systems.com.br>
2023-08-22 22:29:23 -0300

    feat: view child

--- src/app/output-propertie/output-propertie.component.ts
+++ src/app/output-propertie/output-propertie.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -8,17 +8,23 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class OutputPropertieComponent {
 
   @Input() valor: number = 0;
+
   @Output() mudouValor = new EventEmitter();
+
+  @ViewChild('campoInput', { static: false }) campoValorInput!: ElementRef;
 
   constructor() { }
 
   incrementa() {
-    this.valor++;
+    //console.log(this.campoValorInput.nativeElement.value);
+    this.campoValorInput.nativeElement.value++;
+    //this.valor++;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa() {
-    this.valor--;
+    //this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 }
